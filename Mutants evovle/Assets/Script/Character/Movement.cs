6193ce8a98c0066eb56a8bd460058f95e8ddd455
Darht-Ryan <124800164+Darht-Ryan@users.noreply.github.com>
2023-03-10 13:47:06 +0100

    Movement script
    heb het script aangemaakt

---
+++ Mutants evovle/Assets/Script/Character/Movement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float hor;
+    public float ver;
+    public float moveSpeed;
+    public Vector3 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
+
+        moveDirection.x = hor;
+        moveDirection.z = ver;
+
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+    }
+}
