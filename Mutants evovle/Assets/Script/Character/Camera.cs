8904876a73cb171fde7131ab6c1e49ff3e2ca471
Atmojomojo <mairo@samajo.nl>
2023-03-27 00:07:51 +0200

    Conversation scripts + RO scene
    Conversation manager
    SO Conversation
    Movement + Camera script update
    Raycast

--- Mutants evovle/Assets/Script/Character/Camera.cs
+++ Mutants evovle/Assets/Script/Character/Camera.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+    // concam = conversationcamera
+    // Conversationmanager is nodig om de camera switch te laten werken
+    public Conversationmanager conman;
+    public Camera cam;
+    public Camera concam;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam.enabled = true;
+        concam.enabled = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        
+        if (conman.conbool == false)
+        {
+            // zet hier de code in die voor het draaien van de camera zorgt (RO)
+            cam.enabled = true;
+            concam.enabled = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        if (conman.conbool == true)
+        {
+            cam.enabled = false;
+            concam.enabled = true;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+
     }
 }
---
+++ Mutants evovle/Assets/Script/Character/Raycast.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    public RaycastHit hit;
+    public Conversationmanager conman;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 10))
+        {
+            if (conman.conend == false)
+            {
+                if (hit.transform.tag == "Conversation")
+                {
+                    if (Input.GetMouseButton(0))
+                    {
+                        conman.constart = true;
+                        conman.conbool = true;
+                    }
+                }
+            }
+            
+        }    
+
+        
+    }
+}
