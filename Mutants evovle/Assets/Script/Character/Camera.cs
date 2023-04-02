1505a5a0a9abf99c895adb069e1daced371376d6
Darht-Ryan <124800164+Darht-Ryan@users.noreply.github.com>
2023-04-02 23:22:30 +0200

    added pick up!
    pick up script is gemakkt en werkt.
    de deur zit in mijn scene samen met een werkende animatie.
    character valt niet meer.
    3rd person camera script is toegevoed aan de script genaamd Camera.

--- Mutants evovle/Assets/Script/Character/Camera.cs
+++ Mutants evovle/Assets/Script/Character/Camera.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+    //RO
     // concam = conversationcamera
     // Conversationmanager is nodig om de camera switch te laten werken
     public Conversationmanager conman;
     public Camera cam;
     public Camera concam;
+
+    //Ryan
+    public float RotationSpeed = 1;
+    public Transform Target;
+    public Transform Player;
+    public float mouseX;
+    public float mouseY;
     // Start is called before the first frame update
     void Start()
     {
+        //RO
         cam.enabled = true;
         concam.enabled = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -20,8 +29,17 @@ public class Camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        
+        //Ryan
+        mouseX += Input.GetAxis("Mouse X") * RotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * RotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(Target);
+
+        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        Player.rotation = Quaternion.Euler(0, mouseX, 0);
+
+        //RO
         if (conman.conbool == false)
         {
             // zet hier de code in die voor het draaien van de camera zorgt (RO)
--- Mutants evovle/Assets/Script/Character/Raycast.cs
+++ Mutants evovle/Assets/Script/Character/Raycast.cs
@@ -6,18 +6,43 @@ public class Raycast : MonoBehaviour
 {
     public RaycastHit hit;
     public Conversationmanager conman;
+    public GameObject keyCard;
+    public bool isKeyInInv;
+    public Animator animator;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Ryan
         if (Physics.Raycast(transform.position, transform.forward, out hit, 10))
         {
+            if (hit.transform.tag == "keycard")
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    keyCard.SetActive(false);
+                    isKeyInInv = true;
+                }
+            }
+
+            if (isKeyInInv == true)
+            {
+                if (hit.transform.tag == "keyslot")
+                {
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        animator.SetBool("Dooropen", true);
+                    }
+                }
+            }
+
+            //RO
             if (conman.conend == false)
             {
                 if (hit.transform.tag == "Conversation")
