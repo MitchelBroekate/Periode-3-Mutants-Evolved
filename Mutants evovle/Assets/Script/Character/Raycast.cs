a3024d703431c7900362b10c0fe1925a30ff8553
Darht-Ryan <124800164+Darht-Ryan@users.noreply.github.com>
2023-04-05 13:57:41 +0200

    added code cubes
    de cubes met de code en de kleur zitten in mijn scene en kan je ook al op pakken waarna de code wordt geprint.

--- Mutants evovle/Assets/Script/Character/Raycast.cs
+++ Mutants evovle/Assets/Script/Character/Raycast.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Raycast : MonoBehaviour
@@ -7,7 +5,12 @@ public class Raycast : MonoBehaviour
     public RaycastHit hit;
     public Conversationmanager conman;
     public GameObject keyCard;
+    public GameObject code1;
+    public GameObject code2;
+    public GameObject code3;
+    public GameObject code4;
     public bool isKeyInInv;
+    public bool codeInInv;
     public Animator animator;
     public Collider door;
     
@@ -15,6 +18,10 @@ public class Raycast : MonoBehaviour
     void Start()
     {
         keyCard.SetActive(false);
+        code1.SetActive(true);
+        code2.SetActive(true);
+        code3.SetActive(true);
+        code4.SetActive(true); 
     }
 
     // Update is called once per frame
@@ -23,6 +30,42 @@ public class Raycast : MonoBehaviour
         //Ryan
         if (Physics.Raycast(transform.position, transform.forward, out hit, 10))
         {
+            if (hit.transform.tag == "Red1")
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    code1.SetActive(false);
+                    print("red1");
+                }
+            }
+
+            if (hit.transform.tag == "Green2")
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    code2.SetActive(false);
+                    print("green2");
+                }
+            }
+
+            if (hit.transform.tag == "Yellow3")
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    code3.SetActive(false);
+                    print("yellow3");
+                }
+            }
+
+            if (hit.transform.tag == "Blue4")
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    code4.SetActive(false);
+                    print("blue4");
+                }
+            }
+
             if (hit.transform.tag == "keycard")
             {
                 if (Input.GetKeyDown(KeyCode.E))
