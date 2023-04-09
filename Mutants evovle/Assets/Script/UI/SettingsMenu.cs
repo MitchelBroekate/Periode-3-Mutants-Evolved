61152435eb253ea73027bd0072e0a47dae00a66a
MitchelBroekate <mitchelb11@outlook.com>
2023-04-09 21:01:01 +0200

    audio playerprefs
    audio word opgeslagen

--- Mutants evovle/Assets/Script/UI/SettingsMenu.cs
+++ Mutants evovle/Assets/Script/UI/SettingsMenu.cs
@@ -9,18 +9,27 @@ public class SettingsMenu : MonoBehaviour
     public AudioMixer audioMixerSFX;
     public AudioMixer audioMixerMusic;
     public AudioMixer audioMixerDialogue;
+    public float S;
+    public float M;
+    public float D;
  
     public void SetVolumeS(float volumeS)
     {
-        audioMixerSFX.SetFloat("SFX", volumeS);
+        PlayerPrefs.SetFloat("SFXp", volumeS);
+        S = PlayerPrefs.GetFloat("SFXp");
+        audioMixerSFX.SetFloat("SFX", S);
     }
     public void SetVolumeM(float volumeM)
     {
-        audioMixerMusic.SetFloat("Music", volumeM);
+        PlayerPrefs.SetFloat("Musicp", volumeM);
+        M = PlayerPrefs.GetFloat("Musicp");
+        audioMixerMusic.SetFloat("Music", M);
     }
     public void SetVolumeD(float volumeD)
     {
-        audioMixerDialogue.SetFloat("Dialogue", volumeD);
+        PlayerPrefs.SetFloat("Dialoguep", volumeD);
+        D = PlayerPrefs.GetFloat("Dialoguep");
+        audioMixerMusic.SetFloat("Dialogue", D);
     }
 
 
