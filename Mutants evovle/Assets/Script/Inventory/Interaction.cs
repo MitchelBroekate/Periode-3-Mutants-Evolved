756a18845357bd0158eef6be9f622634cb0e2286
Julian Dijkhof <97089833@st.deltion.nl>
2023-04-13 13:21:38 +0200

    inv script
    code voor het oppakken van het papier voor in de UI

---
+++ Mutants evovle/Assets/Script/Inventory/Interaction.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    public RaycastHit hit;
+    public GameObject inventory;
+    public bool ininv = false;
+    public GameObject keycard;
+    public bool pKeycard = false;
+    public bool paper1 = false;
+    public GameObject code1;
+    public bool paper2 = false;
+    public GameObject code2;
+    public bool paper3 = false;
+    public GameObject code3;
+    public bool paper4 = false;
+    public GameObject code4;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        { 
+            if(ininv == true)
+            {
+                inventory.SetActive(false);
+                ininv = false;
+            }
+            else
+            {
+                inventory.SetActive(true);
+                ininv = true;
+            }
+        }
+        if(hit.transform.tag == "Keycard")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                pKeycard = true;
+
+                if (pKeycard == true)
+                {
+                    keycard.SetActive(true);
+                }
+                else
+                {
+                    keycard.SetActive(false);
+                    
+                }
+            }
+        }
+        if (hit.transform.tag == "Papier (1)")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                paper1 = true;
+
+                if (paper1 == true)
+                {
+                    code1.SetActive(true);
+                }
+                else
+                {
+                    code1.SetActive(false);
+
+                }
+            }
+        }
+        if (hit.transform.tag == "Papier (2)")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                paper2 = true;
+
+                if (paper2 == true)
+                {
+                    code2.SetActive(true);
+                }
+                else
+                {
+                    code2.SetActive(false);
+
+                }
+            }
+        }
+        if (hit.transform.tag == "Papier (3)")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                paper3 = true;
+
+                if (paper3 == true)
+                {
+                    code3.SetActive(true);
+                }
+                else
+                {
+                    code3.SetActive(false);
+
+                }
+            }
+        }
+        if (hit.transform.tag == "Papier (4)")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                paper4 = true;
+
+                if (paper4 == true)
+                {
+                    code4.SetActive(true);
+                }
+                else
+                {
+                    code4.SetActive(false);
+
+                }
+            }
+        }
+    }
+}
